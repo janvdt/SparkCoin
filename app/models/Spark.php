1db4f4d51e0cb4f2fde019f12e7c9d08bc80fc24
Jan Van der Taelen <janvandertaelen@gmail.com>
2014-03-29 21:39:48 +0100

    profile.upload

---
+++ app/models/Spark.php
@@ -0,0 +1,6 @@
+<?php
+
+class Spark extends BaseModel {
+
+
+}
--- app/views/instance/header.blade.php
+++ app/views/instance/header.blade.php
@@ -5,4 +5,7 @@
 		<li>{{HTML::link('projects/create', 'Create Project')}}</li>
 		<li>{{HTML::link('dashboard', 'My Account')}}</li>
 	</ul>
+	<div>
+		<p>{{Auth::user()->firstname}} {{Auth::user()->lastname}}</p>
+	<img class="barpic" src="{{ url(Auth::user()->profileUser()->getImagePathname()) }}" width="25" alt="">
 </div>
